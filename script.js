3e2fec3943dd5a8f3bfe7eaa71bcbfd62099d5e3
Havanur <havanurtosun0618@gmail.comhavanurtosun0618@gmail.com>
2021-06-30 14:58:36 -0400

    second commit

--- script.js
+++ script.js
@@ -1,21 +1,69 @@
-// let num1 = parseInt(prompt("please enter a number"));
+let num1 = parseInt(prompt("please enter a number"));
 
-// let num2 = parseInt(prompt("please enter a number"));
+let num2 = parseInt(prompt("please enter a number"));
+
+let opr = prompt("please enter the operator");
 
 
 
-// const calculate = [num1, operation, num2] ;
-//     switch (operation) {
-//       case "+":
-//          return +num1 + +num2;
-//       case "/":
-//          return +num1 / +num2;
-//       case "*":
-//          return +num1 * +num2;
-//       case "%":
-//          return +num1 % +num2;
-//       default:
-//          return 0;
+const calculate = [num1, operation, num2] ;
+    switch (operation) {
+      case "+":
+         return num1 + num2;
+      case "/":
+         return num1 / num2;
+      case "*":
+         return num1 * num2;
+      case "%":
+         return num1 % num2;
+      case "-":
+         return num1 - num2;
+      default:
+         return 0;
+}
+
+// console
+
+// class Calculator {
+//    constructor(previousOperandTextElement, currentOperandTextElement) {
+//       this.previousOperandTextElement = previousOperandTextElement
+//       this.currentOperandTextElement = currentOperandTextElement
+//    }
+
+
+//    clear() {
+//       this.currentOperand = ''
+//       this.previousOperand = ''
+//       this.operation = undefined
+//    }
+
+//    delete() {
+
+//    }
+
+//    appendNumber(number) {
+
+//    }
+
+//    chooseOperation(operation) {
+
+//    }
+
+//    compute() {
+
+//    }
+
+//    updateDisplay() {
+
+//    }
 // }
 
-console
+
+
+// const numberButtons = document.querySelectorAll('[data-number]')
+// const numberButtons = document.querySelectorAll('[data-operation]')
+// const equalsButton = document.querySelector('[data-equals]')
+// const deleteButton = document.querySelector('[data-delete]')
+// const allClearButton = document.querySelector('[data-all-clear]')
+// const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+// const currentOperandTextElement  = document.querySelector('[data-current-operand]')
