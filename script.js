0471948a76a42b0e7641e2234abfe2554fb53d55
Havanur <havanurtosun0618@gmail.comhavanurtosun0618@gmail.com>
2021-06-30 13:43:38 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// let num1 = parseInt(prompt("please enter a number"));
+
+// let num2 = parseInt(prompt("please enter a number"));
+
+
+
+// const calculate = [num1, operation, num2] ;
+//     switch (operation) {
+//       case "+":
+//          return +num1 + +num2;
+//       case "/":
+//          return +num1 / +num2;
+//       case "*":
+//          return +num1 * +num2;
+//       case "%":
+//          return +num1 % +num2;
+//       default:
+//          return 0;
+// }
+
+console
